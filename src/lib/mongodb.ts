f8f98c19b8eeb1b381e05f5a3cedf39e63802864
Tudor Comaparan <tudor.comparan@gmail.com>
2023-04-05 09:30:22 -0600

    adding options to mongodb client (#6)

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,12 +1,12 @@
 // This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
-import {MongoClient} from 'mongodb';
+import {MongoClient, ServerApiVersion} from 'mongodb';
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
 }
 
 const uri = process.env.MONGODB_URI;
-const options = {};
+const options = {useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1};
 
 let client;
 let clientPromise: Promise<MongoClient>;
